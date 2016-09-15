5350e576626126c095dbd22c765cc14b606f7fe2
Dalvan Gomes <dalvangsilva@gmail.com>
2016-09-14 23:24:31 -0300

    removido semaforo da worker

--- app/workers/webmotors.rb
+++ app/workers/webmotors.rb
@@ -1,11 +1,8 @@
 class Webmotors
 	include Sidekiq::Worker
 	def perform
-		s = Redis::Semaphore.new(:webmotorsmakers, :host => "localhost")
-		s.lock do	
-			Webmotorsmakers.fetch.each do |make|
-					Webmotorsmodels.fetch make["Id"]
-			end
+		Webmotorsmakers.fetch.each do |make|
+				Webmotorsmodels.fetch make["Id"]
 		end
 	end
 end
